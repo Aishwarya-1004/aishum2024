f1b401c5df1ada66dd5ced9727f7e7205a5cb17d
aishwarya <ishhhu.10@gmail.com>
2024-12-28 22:34:22 +0530

    adding

---
+++ looping.js
@@ -0,0 +1,12 @@
+array = [20,30,40,50,"a","kec","true"]
+console.log(array)
+
+//for in loop ..
+for(var a in array){
+    console.log("elements in index",a);
+}
+
+//for of loop...
+for(var b of array){
+    console.log(b);
+}
